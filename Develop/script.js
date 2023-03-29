60b00527ef98d858aeef0fbf2aeb662dc12fd863
Elfelfa <koltbodzo@gmail.com>
2023-03-29 13:24:23 -0400

    Created requests for user input.

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
 // Indexes:
 // 0-9: Numbers (10 total indexes)
 // 10-35: Lowercase letters (26 total indexes)
@@ -14,14 +13,56 @@ const characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 
                   'Y', 'Z', ' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',',
                   '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_',
                   '`', '{', '|', '}', '~'];
-                  
-                  
+
 // Write password to the #password input
 function writePassword() 
 {
-  var password = generatePassword();
+  var numChars = 0;
+  var num, lower, upper, special;
+
+  //Request a number from the user between 8-128. Loop the request if the entry is not valid.
+  while (true)
+  {
+    numChars = prompt("Please specify the number of characters you'd like in your new password. Minimum: 8, Maximum: 128");
+
+    //Checks to make sure the input from the user is a valid number
+    if (!isNaN(numChars))
+    { 
+      //Converts the input from a string to a number
+      numChars = parseInt(numChars);
+
+      //If the number is between 8 and 128 the program continues. If not, it loops the request.
+      if ((numChars >= 8) && (numChars <= 128))
+      {
+        break;
+      }
+    }
+
+    alert("Invalid entry");
+  }
+
+  //Request the user to confirm if they would like to add numbers, lowercase, uppercase, and special characters
+  while(true)
+  {
+    num = confirm("Would you like to include numbers in your new password?");
+    lower = confirm("Would you like to include lowercase letters in your new password?");
+    upper = confirm("Would you like to include uppercase letters in your new password?");
+    special = confirm("Would you like to include special characters in your new password?");
+
+    //If the user said no to everything, redo the request stating that the user must pick at least one.
+    if ((true in {num, lower, upper, special}) === false)
+    {
+      break;
+    }
+
+    alert("You must pick at least one!");
+  }
+
+  //Generate a password using the user's input values
+  var password = generatePassword(numChars, num, lower, upper, special);
   var passwordText = document.querySelector("#password");
 
+  //Replaces the placeholder text on the page with the new password.
   passwordText.value = password;
 
 }
@@ -60,7 +101,7 @@ function generatePassword(c, n, l, u, s)
 
   for (c; c > 0; c--)
   {
-    newPassword += selectedCharacters[Math.floor(Math.random() * selectedCharacters.length())];
+    newPassword += selectedCharacters[Math.floor(Math.random() * selectedCharacters.length)];
   }
 
   return newPassword;
